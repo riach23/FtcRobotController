06f46ceccfc07c1a12da97f51a32c2e0a8118be2
riach23 <114191576+riach23@users.noreply.github.com>
2022-09-23 16:48:22 -0500

    edited

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,18 @@
+package org.firstnspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp()
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+       telemetry.addData("Hello", "World");
+
+    }
+
+    public void loop(){
+
+    }
+}
