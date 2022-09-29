a35ce6da00c7db42fb3cb75b67ef05284fe8df9b
riach23 <114191576+riach23@users.noreply.github.com>
2022-09-29 16:51:25 -0500

    edited

---
+++ TeamCode/src/main/java/TouchSensorOpMode2.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcommrobotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard2;
+
+@TeleOp
+public class TouchSensorOpMode2 extends OpMode {
+    ProgrammingBoard2 board = new ProgrammingBoard2 ();
+
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+
+    @Override
+    public void loop(){
+        telemetry.addData("Touch pressed", board.isTouchSensorPressed());
+    }
+}
