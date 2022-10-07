d11662d68785212314d24b1e1400bd3d8324177e
riach23 <114191576+riach23@users.noreply.github.com>
2022-10-07 17:38:28 -0500

    saved

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ProgrammingBoard4.java
@@ -0,0 +1,16 @@
+package org.firstinpires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class ProgrammingBoard4 {
+    private DigitalChannel touchSensor;
+    private DcMotor motor;
+
+    private double ticketsPerRotation;
+
+    public void init(HardwareMap hwMap) {
+        touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor")
+    }
+}
